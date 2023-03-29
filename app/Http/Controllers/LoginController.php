ee4956052c09668114bee311f41f0de74e437fd1
pms007 <pymyoswe007@gmail.com>
2023-03-29 10:59:34 +0700

    fix login api

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,46 +5,22 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\LoginRequest;
+use App\Http\Resources\LoginResource;
 
 class LoginController extends Controller
 {
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
-        if (!$request->email) {
-            return response()->json([
-                'status'  => 422,
-                'message' => 'email is required'
-            ]);
-        }
-        
-        if(strlen($request->email) < 6) {
-            return response()->json([
-                'status'  => 422,
-                'message' => 'email is invalid'
-            ]);
-        }
-    
-        if (!$request->password) {
-            return response()->json([
-                'status'  => 422,
-                'message' => 'password is required'
-            ]);
-        }
-        if(strlen($request->password) < 8) {
-            return response()->json([
-                'status'  => 422,
-                'message' => 'password is invalid'
-            ]);
-        }
-    
         $user = User::where('email', $request->email)->first();
+        
         if (!$user) {
             return response()->json([
                 'status'  => 404,
                 'message' => 'Model not found.'
             ]);
         }
-    
+        
         if (!Hash::check($request->password, $user->password)) {
             return response()->json([
                 'status'  => 404,
@@ -53,8 +29,9 @@ class LoginController extends Controller
         }
         
         return response()->json([
-            'user' => $user,
-            'token' => $user->createToken('User-Token')->plainTextToken
+            'result' => new LoginResource($user),
+            'status' => 200,
+            'message'=> 'Success'
         ]);
     }
 }
